f56465a7d5a5672a58d7428841b5f3e9ef2ff3a4
MattieMonster <mattiemonster1@hotmail.com>
2016-09-25 21:23:47 +0100

    Added megalib

---
+++ Megalib/src/engine/MegaEngine.java
@@ -0,0 +1,9 @@
+package engine;
+
+public class MegaEngine {
+
+	public MegaEngine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Megalib/src/engine/MegaMath.java
@@ -0,0 +1,9 @@
+package engine;
+
+public class MegaMath {
+
+	public MegaMath() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
